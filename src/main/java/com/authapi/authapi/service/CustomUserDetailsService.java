9f9bf566e0b0990077ba36a87c264ca6cc522c87
SanJohan <johansantiago56@gmail.com>
2026-04-23 14:41:50 -0500

    creating CustomUserDatil service and its method extended by loadUserByUsername, and add password encoder in securityconfig

---
+++ src/main/java/com/authapi/authapi/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.authapi.authapi.service;
+
+import com.authapi.authapi.model.User;
+import com.authapi.authapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usario no encontrad" + username));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRoles().stream()
+                        .map( role -> new SimpleGrantedAuthority(role.name()))
+                        .toList())
+                .build();
+    }
+}
